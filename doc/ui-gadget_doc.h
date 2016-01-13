a4e7ff94e5db5fa7d945a76732d260776117979d
SukHyung, Kang <shine.kang@samsung.com>
2016-01-14 08:47:17 +0900

    Add doc files for API references
    Change-Id: Ib25f218e3be5f74eee461399c45b2bd922a91ce0
    Signed-off-by: SukHyung, Kang <shine.kang@samsung.com>

---
+++ doc/ui-gadget_doc.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __TIZEN_CORE_LIB_UI_GADGET_DOC_H__
+#define __TIZEN_CORE_LIB_UI_GADGET_DOC_H__
+
+/**
+ * @defgroup CORE_LIB_GROUP_UI_GADGET_MODULE UI Gadget
+ * @brief  The UI gadget is a visual component providing views(or features) of other applications.
+ * @ingroup CAPI_APPLICATION_FRAMEWORK
+ *
+ * @section CORE_LIB_GROUP_UI_GADGET_MODULE_HEADER Required Header
+ *   \#include <ui-gadget.h>
+ *
+ * @section CORE_LIB_GROUP_UI_GADGET_MODULE_OVERVIEW Overview
+ * The UI gadget is a visual component providing views(or features) of other applications.
+ * Because UI gadgets are supposed to deliver most commonly used features that the platform can natively support,
+ * developers can avoid unnecessary massive code writing using UI gadgets.
+ * Hence an UI gadget eventually includes logics to handle simple request.
+ * UI gadget is able to be managed by UI gadget library
+ */
+
+/**
+ * @defgroup CORE_LIB_GROUP_UI_GADGET_MODULE_MODULE UI Gadget Module
+ * @brief
+ * @ingroup CORE_LIB_GROUP_UI_GADGET_MODULE
+ *
+ * @section CORE_LIB_GROUP_UI_GADGET_MODULE_MODULE_HEADER Required Header
+ *   \#include <ui-gadget-module.h>
+ */
+
+#endif /* __TIZEN_CORE_LIB_UI_GADGET_DOC_H__ */
