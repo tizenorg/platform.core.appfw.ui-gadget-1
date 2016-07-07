a87c1cb3daae01e318b8225d23662151927ae3d0
Semun Lee <sm79.lee@samsung.com>
2016-07-07 20:10:16 +0900

    Fix crash of ug-client apps
    dlopen ug shared library file again to prevent crash while terminating app.
    In some case, unloaded memory could be accessed by other libaries clean up routine.
    
    Fix double free of ad.name
    
    Change-Id: I612c318ccb798d76a0ac8b77ab8f23dbe2b1e974
    Signed-off-by: Semun Lee <sm79.lee@samsung.com>

--- src/ug.c
+++ src/ug.c
@@ -377,12 +377,20 @@ UG_API int ug_disable_effect(ui_gadget_h ug)
 
 UG_API int ug_is_installed(const char *name)
 {
+	int ret = 1;
+	char *ug_file_path;
+
 	if (name == NULL) {
 		_ERR("name is null");
 		return -1;
 	}
 
-	return ug_exist(name);
+	if (!ug_module_get_file_path(name, &ug_file_path))
+		ret = 0;
+
+	free(ug_file_path);
+
+	return ret;
 }
 
 #ifdef ENABLE_UG_CREATE_CB
